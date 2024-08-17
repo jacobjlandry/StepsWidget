81505a5d1fa395021191b67e49adaf9b4b05ee49
Jacob Landry <landry.jacob@gmail.com>
2024-08-16 22:37:59 -0400

    Initial Commit

---
+++ StepsWidget/StepsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsWidgetApp.swift
+//  StepsWidget
+//
+//  Created by Jacob Landry on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct StepsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
